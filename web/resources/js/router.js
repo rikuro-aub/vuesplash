0f29989aa45c66dbf16626994ad3413dd5b0475b
3no3 <ulis7524@gmail.com>
2020-03-17 10:35:56 +0900

    VueRouterでルーティングを設定

---
+++ web/resources/js/router.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+// ページコンポーネントをインポートする
+import PhotoList from './pages/PhotoList.vue'
+import Login from './pages/Login.vue'
+
+// VueRouterプラグインを使用する
+Vue.use(VueRouter)
+
+// パスとコンポーネントのマッピング
+const routes = [
+    {
+        path: '/',
+        component: PhotoList
+    },
+    {
+        path: '/login',
+        component: Login
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history',    // rootの#を省略する
+    routes
+})
+
+// app.jsでインポートするため
+export default router
